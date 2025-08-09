02836744ac3fdc65e4c2cb77004820fa08e7d449
williamlam31 <williamlam31@gmail.com>
2025-08-09 14:56:31 -0400

    Update hotel_app.py

--- hotel_app.py
+++ hotel_app.py
@@ -426,22 +426,16 @@ def show_Price_Prediction(data):
         with col1:
             hotel = st.selectbox("Hotel Type", options=['Resort Hotel', 'City Hotel'])
             lead_time = st.number_input("Lead Time (days)", min_value=0, max_value=500, value=50)
-            arrival_year = st.number_input("Arrival Year", min_value=2015, max_value=2025, value=2024)
+            special_requests = st.text_input("Special Requests", value="0", placeholder="Enter number (0-10)")
             arrival_month = st.selectbox("Arrival Month",
                                        options=['January', 'February', 'March', 'April', 'May', 'June',
-                                               'July', 'August', 'September', 'October', 'November', 'December'])
-            arrival_week = st.number_input("Week Number", min_value=1, max_value=53, value=26)
-        
+                                               'July', 'August', 'September', 'October', 'November', 'December'])        
         with col2:
-            arrival_day = st.number_input("Day of Month", min_value=1, max_value=31, value=15)
             weekend_nights = st.number_input("Weekend Nights", min_value=0, max_value=10, value=1)
-            week_nights = st.number_input("Week Nights", min_value=0, max_value=20, value=2)
-            adults = st.number_input("Adults", min_value=0, max_value=10, value=2)
-            children = st.number_input("Children", min_value=0, max_value=5, value=0)
+            total_nights = st.number_input("Total Nights", min_value=0)
+            total_guests = st.number_input("Adults", value =0)
         
         with col3:
-            babies = st.number_input("Babies", min_value=0, max_value=3, value=0)
-            meal = st.selectbox("Meal Plan", options=['BB', 'FB', 'HB', 'SC', 'Undefined'])
             market_segment = st.selectbox("Market Segment",
                                         options=['Direct', 'Corporate', 'Online TA', 'Offline TA/TO',
                                                 'Complementary', 'Groups', 'Aviation'])
